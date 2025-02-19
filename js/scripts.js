1ba20e5741c54090304939e553ff6f441e1bc2b8
jeffryatf <jeffryaturf@hotmail.com>
2025-02-19 14:30:29 +0700

    New Modal
    clickable item with modal

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,7 @@
 
   function light(card, x, y) {
       let angle = (Math.atan2(y, x) * 180) / Math.PI - 90;
-      let gloss = card.querySelector('.card__gloss');
+      let gloss = card.querySelector('.card-gloss');
 
       gloss.style.background = 'linear-gradient(' + angle + 'deg, rgba(255, 255, 255,' + y / height * .9 + ') 0%, rgba(255, 255, 255, 0) 80%)';
   }
@@ -39,9 +39,54 @@
           ry = (y / height) * -force;
 
       card.style.transform = 'rotateY(' + (rx) + 'deg) rotateX(' + (ry) + 'deg)';
-      // let content = card.querySelector('.card__content');
-      // content.style.transform = 'translateX(' + (rx * .75) + 'px) translateY(' + (ry * .75) + 'px)';
   }
+
+  var modal = document.getElementById("myModal");
+  var span = document.getElementsByClassName("close")[0];
+
+  function openModal(title, description, image, buttonText, buttonVisible, buttonLink) {
+    modal.querySelector("h1").textContent = title;
+    modal.querySelector("p").innerHTML = description.replace(/\n/g, '<br>');
+    modal.querySelector("img").src = image;
+    var modalButton = modal.querySelector(".modal-button");
+    modalButton.textContent = buttonText;
+    modalButton.style.display = buttonVisible ? 'block' : 'none';
+    modalButton.onclick = function() {
+      window.location.href = buttonLink;
+    };
+    modal.classList.add("show");
+    setTimeout(function() {
+      modal.style.opacity = "1";
+    }, 10);
+  }
+
+  span.onclick = function() {
+    modal.style.opacity = "0";
+    setTimeout(function() {
+      modal.classList.remove("show");
+    }, 300); 
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.opacity = "0";
+      setTimeout(function() {
+        modal.classList.remove("show");
+      }, 300);
+    }
+  }
+
+  document.querySelectorAll(".modalclick").forEach(function(modalClickItem) {
+    modalClickItem.addEventListener("click", function() {
+      var title = modalClickItem.getAttribute("data-title");
+      var description = modalClickItem.getAttribute("data-description");
+      var image = modalClickItem.getAttribute("data-image");
+      var buttonText = modalClickItem.getAttribute("data-button-text") || "";
+      var buttonVisible = modalClickItem.getAttribute("data-button-visible") === "true";
+      var buttonLink = modalClickItem.getAttribute("data-button-link") || "#";
+      openModal(title, description, image, buttonText, buttonVisible, buttonLink);
+    });
+  });
 
   init();
 })();
